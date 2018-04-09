95a31337f6a0ca47fa707a94dbb2420f4b16dfe2
Daniel Nascimento Barroso <barrozim@gmail.com>
2018-04-09 08:21:47 -0300

    Base do prpojeto
    Base do projeto

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OftalClinic
+
+OftalClinic é uma aplicação web desenvolvida para cadeira Requisitos de Sistemas da Universidade Estácio de Sá (FIC - Moreira Campos)
+
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'    
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/public"),
+    compress: true,
+    port: 8080
+  }
+  ,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
