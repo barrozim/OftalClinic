26eed3989fd0b67398baf791af0868917bef4fb6
Daniel Barroso <barrozim@gmail.com>
2018-05-04 00:13:35 -0300

    Remoção dos dados do cliente para tela de refresh.

--- frontend/src/cliente/clienteActions.jsx
+++ frontend/src/cliente/clienteActions.jsx
@@ -7,30 +7,25 @@ const URL = 'http://35.231.93.88:3003/api/oftalclinic/cliente'
 const INITIAL_VALUES = { hideFormulario: false, list: [] }
 
 export function getList() {
-    return function action(dispatch)
-    {
-        //dispatch({ type: 'CLIENTE_FETCHED' })
-
+    return function action(dispatch) {
         const request = axios.get(`${URL}?sort=-dataatualizacao`)
 
         return request.then(
             response => dispatch(fetchOffersSuccess(response)),
             err => dispatch(fetchOffersError(err))
-          );
+        );
 
     }
 }
 
-export function fetchOffersSuccess(response)
-{
+export function fetchOffersSuccess(response) {
     return {
         type: 'CLIENTE_FETCHED',
         payload: response
     }
 }
 
-export function fetchOffersError(err)
-{
+export function fetchOffersError(err) {
     return {
         type: 'CLIENTE_FETCHED_ERROR',
         payload: err
@@ -44,44 +39,43 @@ export function modifyHideFormulario() {
     }
 }
 
-export function remove(obj){
-    axios.delete(`${URL}/${obj._id}`)
-    .then(resp => {
-        toastr.success('Sucesso', 'Operação Realizada com sucesso.')
-        dispatch([
-            init()
-        ])
-    })
-    .catch(e => {
-        Object.keys(e.response.data.errors).forEach(error => toastr.error('Erro',e.response.data.errors[error].message))
-    })
+export function remove(obj) {
+    return function action(dispatch) {
+        axios.delete(`${URL}/${obj._id}`)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+                dispatch(init())
+            })
+            .catch(e => {
+                Object.keys(e.response.data.errors).forEach(error => toastr.error('Erro', e.response.data.errors[error].message))
+            })
+    }
 }
 
-export function create(values)
-{
+export function create(values) {
     return dispatch => {
-     axios.post(URL, values)
-    .then(resp => {
-        toastr.success('Sucesso', 'Operação Realizada com sucesso.')
-        dispatch([
-            init(),
-            modifyHideFormulario()
-        ])
-    })
-    .catch(e => {
-        Object.keys(e.response.data.errors).forEach(error => toastr.error('Erro',e.response.data.errors[error].message))
-    })
+        axios.post(URL, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+                dispatch([
+                    init(),
+                    modifyHideFormulario()
+                ])
+            })
+            .catch(e => {
+                Object.keys(e.response.data.errors).forEach(error => toastr.error('Erro', e.response.data.errors[error].message))
+            })
 
     }
 }
 
 
-export function init(){
+export function init() {
     return [
         resetForm('clienteForm'),
         getList(),
         initialize('clientForm', INITIAL_VALUES)
-        
+
     ]
 
 }
